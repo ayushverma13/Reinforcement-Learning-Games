94b5dbbb4300fcacba30416750caa34bc73cb91b
ayushverma13 <46970571+ayushverma13@users.noreply.github.com>
2020-07-16 16:12:35 +0530

    Add files via upload

---
+++ Reinforcement Learning/paddlegame.py
@@ -0,0 +1,11 @@
+import turtle
+
+win = turtle.Screen()    # Create a screen
+win.title('Paddle')      # Set the title to paddle
+win.bgcolor('black')     # Set the color to black
+win.tracer(0)
+win.setup(width=600, height=600)   # Set the width and height to 600
+while True:
+     win.update()        # Show the scree continuously 
+
+done()
